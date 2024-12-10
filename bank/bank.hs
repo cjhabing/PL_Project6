140e7068bb98640a513a268ed5d1ac54c5e8440c
Chaitanya Jha <cjha@cs.binghamton.edu>
2024-12-09 20:24:01 -0500

    DONE

--- bank/bank.hs
+++ bank/bank.hs
@@ -1,19 +1,16 @@
 module Bank where
 
--- Define the BankOp type to encapsulate stateful computations
 newtype BankOp a = BankOp { runBankOpWithState :: Float -> (a, Float) }
 
--- Define how to run a BankOp, starting from an initial balance of 0
 runBankOp :: BankOp a -> a
 runBankOp (BankOp op) = fst (op 0)
 
--- Functor instance for BankOp
 instance Functor BankOp where
     fmap f (BankOp op) = BankOp (\balance -> 
         let (result, newBalance) = op balance
         in (f result, newBalance))
 
--- Applicative instance for BankOp
+
 instance Applicative BankOp where
     pure x = BankOp (\balance -> (x, balance))
     (BankOp f) <*> (BankOp x) = BankOp (\balance ->
@@ -21,7 +18,7 @@ instance Applicative BankOp where
             (y, balance2) = x balance1
         in (g y, balance2))
 
--- Monad instance for BankOp
+
 instance Monad BankOp where
     return = pure
     (BankOp op) >>= f = BankOp (\balance -> 
@@ -29,16 +26,13 @@ instance Monad BankOp where
             BankOp nextOp = f result
         in nextOp newBalance)
 
--- Deposit a specified amount
 deposit :: Float -> BankOp ()
 deposit amount = BankOp (\balance -> ((), balance + amount))
 
--- Withdraw a specified amount, allowing overdrafts up to $100
 withdraw :: Float -> BankOp Float
 withdraw amount = BankOp (\balance ->
     let actualWithdrawal = if balance - amount >= -100 then amount else balance + 100
     in (actualWithdrawal, balance - actualWithdrawal))
 
--- Get the current balance
 getBalance :: BankOp Float
 getBalance = BankOp (\balance -> (balance, balance))
